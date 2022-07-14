5b2d41215db71e2772087b48e95e91eaeff62dad
JORDI.NODEJS <jordi.nodejs@gmail.com>
2022-07-14 18:02:06 +0200

    daisyUI config theme

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,5 +1,60 @@
+import { Link } from 'react-router-dom'
 const Login = () => {
-  return <div>Login</div>
+  return (
+    <div data-theme='' className='hero min-h-screen bg-base-200'>
+      <div className='hero-content flex-col lg:flex-row-reverse'>
+        <div className='text-center lg:text-left'>
+          <h1 className='text-5xl font-bold'>Login now!</h1>
+          <p className='py-6'>
+            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi
+            exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.
+          </p>
+        </div>
+        <div className='card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100'>
+          <div className='card-body'>
+            <div className='form-control'>
+              <label className='label'>
+                <span className='label-text'>Email</span>
+              </label>
+              <input type='text' placeholder='email' className='input input-bordered' />
+            </div>
+            <div className='form-control'>
+              <label className='label'>
+                <span className='label-text'>Password</span>
+              </label>
+              <input type='text' placeholder='password' className='input input-bordered' />
+            </div>
+            <div className='form-control mt-6'>
+              <button className='btn btn-primary'>Login</button>
+            </div>
+            <div className='text-sm font-medium text-gray-700 dark:text-gray-400'>
+              Not registered?{' '}
+              <Link to='/register' className='text-purple-700 hover:underline dark:text-purple-500'>
+                Create account
+              </Link>
+            </div>
+            <div className='mt-10 flex justify-center'>
+              <button
+                className='btn btn-ghost  group h-12 px-6 border-2  rounded-full transition duration-300'>
+                <div className='relative flex space-x-4 justify-start'>
+                  <img
+                    src='https://tailus.io/sources/blocks/social/preview/images/google.svg'
+                    className='w-5'
+                    alt='google logo'
+                  />
+                  <span className='block w-max font-semibold tracking-wide text-sm transition duration-300 sm:text-base'>
+                    Continue with Google
+                  </span>
+                </div>
+              </button>
+
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Login
--- src/router/PostApp.jsx
+++ src/router/PostApp.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Welcome from '../components/post/Welcome'
 import Login from '../components/auth/Login'
+import Register from '../components/auth/Register'
 import NotFound from '../components/NotFound'
 
 const PostApp = () => {
@@ -8,7 +9,9 @@ const PostApp = () => {
     <BrowserRouter>
       <Routes>
         <Route path='/auth' element={<Login />} />
-        <Route path='/' element={<Welcome />} />
+        <Route path='/' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/welcome' element={<Welcome />} />
         <Route path='*' element={<NotFound />} />
 
       </Routes>
