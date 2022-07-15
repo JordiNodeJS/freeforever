2f94cfbfffbb4d81bd9bc52cb5689e45949fb854
JORDI.NODEJS <jordi.nodejs@gmail.com>
2022-07-15 19:41:36 +0200

    google sin in

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,18 +1,22 @@
-import { firebase, googleAuthProvider } from '../firebase.config';
+import { firebase, googleAuthProvider } from '../firebase.config'
 import { types } from '../types'
 
 export const startLoginEmailPassword = (email, password) => {
   return dispatch => {
     setTimeout(() => {
-        dispatch( login(123, 'juan') )
-    }, 3500);
+      dispatch(login(123, 'juan'))
+    }, 3500)
   }
 }
 
 export const startGoogleLogin = () => {
   return dispatch => {
-    firebase.auth().signInWithPopup(googleAuthProvider)
-    .then( userCredentials => {console.log(userCredentials)} )
+    firebase
+      .auth()
+      .signInWithPopup(googleAuthProvider)
+      .then(({user}) => {
+        dispatch(login(user.uid, user.displayName))
+      })
   }
 }
 
