85719cbf7be86b18489210a5a7f7c72b1181c71b
JORDI.NODEJS <jordi.nodejs@gmail.com>
2022-07-17 23:04:29 +0200

    loading main route

--- src/router/PostApp.jsx
+++ src/router/PostApp.jsx
@@ -4,36 +4,43 @@ import Welcome from '../components/post/Welcome'
 import Login from '../components/auth/Login'
 import Register from '../components/auth/Register'
 import NotFound from '../components/NotFound'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { firebase } from '../firebase.config'
 import { useDispatch } from 'react-redux'
 import { login } from '../actions/auth'
 
 const PostApp = () => {
+  const [checkAuth, setCheckAuth] = useState(true)
+  const [isLogin, setIsLogin] = useState(false)
+
   const dispatch = useDispatch()
+
   useEffect(() => {
     firebase.auth().onAuthStateChanged(user => {
       if (user?.uid) {
         console.log('user logged in')
-        dispatch( login(user.uid, user.displayName))
-
+        dispatch(login(user.uid, user.displayName))
+        setIsLogin(true)
       } else {
         console.log('user logged out')
+        setIsLogin(false)
       }
+      setCheckAuth(false)
     })
-  }, [])
-  return (
+  }, [dispatch, checkAuth, isLogin])
 
-        <BrowserRouter>
-          <Routes>
-            <Route path='/auth' element={<Login />} />
-            <Route path='/' element={<Login />} />
-            <Route path='/register' element={<Register />} />
-            <Route path='/welcome' element={<Welcome />} />
-            <Route path='*' element={<NotFound />} />
-          </Routes>
-        </BrowserRouter>
-   
+  if (checkAuth) return <h1 className='absolute bottom-0 right-1 text-info'>Waiting...</h1>
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/auth' element={<Login />} />
+        <Route path='/' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/welcome' element={<Welcome />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
