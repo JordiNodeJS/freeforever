c294ee52f4624bb761d78ba000a06c830b47a547
JORDI.NODEJS <jordi.nodejs@gmail.com>
2022-07-18 20:42:14 +0200

    firebase modular

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,52 +1,96 @@
-import { firebase, googleAuthProvider, updateProfile } from '../firebase.config'
+// import { firebase, googleAuthProvider, updateProfile } from '../firebase.config'
 import { types } from '../types'
 import { startLoading, finishLoading, setError } from './ui'
-import { toast } from 'react-toastify';
+import { toast } from 'react-toastify'
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  updateProfile,
+  signInWithPopup,
+  GoogleAuthProvider,
+  signOut 
+} from 'firebase/auth'
 
 export const startLoginEmailPassword = (email, password) => {
   return dispatch => {
     dispatch(startLoading())
 
-    firebase
-      .auth()
-      .signInWithEmailAndPassword(email, password)
-      .then(({ user }) => {
-        dispatch(login(user.uid, user.displayName))
+    const auth = getAuth()
+    signInWithEmailAndPassword(auth, email, password).then(userCredntials => {
+      const user = userCredntials.user
+      dispatch(login(user.uid, user.displayName))
+      dispatch(finishLoading())
+    })
 
-        dispatch(finishLoading())
-      })
-      .catch(err => {
-      
-        dispatch(finishLoading())
-      })
+    // firebase
+    //   .auth()
+    //   .signInWithEmailAndPassword(email, password)
+    //   .then(({ user }) => {
+    //     dispatch(login(user.uid, user.displayName))
+
+    //     dispatch(finishLoading())
+    //   })
+    //   .catch(err => {
+
+    //     dispatch(finishLoading())
+    //   })
   }
 }
 
 export const startRegisterEmailPasswordName = (email, password, name) => {
   return dispatch => {
-    firebase
-      .auth()
-      .createUserWithEmailAndPassword(email, password)
-      .then(async ({ user }) => {
-        await updateProfile(user, {
-          displayName: name,
-        })
+    const auth = getAuth()
+    createUserWithEmailAndPassword(auth, email, password)
+      .then(userCredntials => {
+        const user = userCredntials.user
+        updateProfile(user, { displayName: name })
         dispatch(login(user.uid, user.displayName))
-        console.log(user)
       })
-      .catch( err => {
-        // alert(err.message)
-        toast.error('🦄 ' + err.message);
-        console.log('🦄',err.message)
+      .catch(err => {
         dispatch(setError(err.message))
+        toast.error('🦄 ' + err.message)
       })
+
+    // firebase
+    //   .auth()
+    //   .createUserWithEmailAndPassword(email, password)
+    //   .then(async ({ user }) => {
+    //     await updateProfile(user, {
+    //       displayName: name,
+    //     })
+    //     dispatch(login(user.uid, user.displayName))
+    //     console.log(user)
+    //   })
+    //   .catch(err => {
+    //     // alert(err.message)
+    //     toast.error('🦄 ' + err.message)
+    //     console.log('🦄', err.message)
+    //     dispatch(setError(err.message))
+    //   })
   }
 }
 
-export const startGoogleLogin = () => async dispatch => {
-  const { user } = await firebase.auth().signInWithPopup(googleAuthProvider)
-  dispatch(login(user.uid, user.displayName))
+export const startGoogleLogin = () => dispatch => {
+  const googleAuthProvider = new GoogleAuthProvider()
+  const auth = getAuth()
+  signInWithPopup(auth, googleAuthProvider)
+    .then(({user}) => {
+      console.log(user)
+      dispatch(login(user.uid, user.displayName))
+    })
+    .catch(error => {
+      // Handle Errors here.
+
+      console.log('error ->', error)
+      toast.error( `🦄 ${error.code} ${error.message}`  )
+
+    })
 }
+// export const startGoogleLogin = () => async dispatch => {
+//   const { user } = await firebase.auth().signInWithPopup(googleAuthProvider)
+//   dispatch(login(user.uid, user.displayName))
+// }
 
 export const login = (uid, displayName) => ({
   type: types.login,
@@ -59,17 +103,30 @@ export const login = (uid, displayName) => ({
 export const logout = () => ({
   type: types.logout,
 })
+
 export const startLogout = () => {
   return dispatch => {
-    firebase
-      .auth()
-      .signOut()
-      .then(() => {
-        dispatch(logout())
-      })
-      .catch(err => {
-        console.log(err)
-      })
+    const auth = getAuth()
+    signOut(auth)
+    .then(() => {
+      dispatch(logout())
+    }
+    ).catch(err => {
+      toast.error('🦄 ' + err.message)
+    }
+    )
   }
 }
-
+// export const startLogout = () => {
+//   return dispatch => {
+//     firebase
+//       .auth()
+//       .signOut()
+//       .then(() => {
+//         dispatch(logout())
+//       })
+//       .catch(err => {
+//         console.log(err)
+//       })
+//   }
+// }
--- src/router/PostApp.jsx
+++ src/router/PostApp.jsx
@@ -6,6 +6,7 @@ import Register from '../components/auth/Register'
 import NotFound from '../components/NotFound'
 import { useEffect, useState } from 'react'
 import { firebase } from '../firebase.config'
+import { getAuth, onAuthStateChanged } from "firebase/auth"
 import { useDispatch } from 'react-redux'
 import { login } from '../actions/auth'
 import SideBar from '../components/auth/SideBar'
@@ -32,6 +33,20 @@ const PostApp = () => {
       setCheckAuth(false)
     })
   }, [dispatch, checkAuth, isLogin])
+
+  // useEffect(() => {
+  //   firebase.auth().onAuthStateChanged(user => {
+  //     if (user?.uid) {
+  //       console.log('user logged in')
+  //       dispatch(login(user.uid, user.displayName))
+  //       setIsLogin(true)
+  //     } else {
+  //       console.log('user logged out')
+  //       setIsLogin(false)
+  //     }
+  //     setCheckAuth(false)
+  //   })
+  // }, [dispatch, checkAuth, isLogin])
 
   if (checkAuth) return <h1 className='absolute bottom-0 right-1 text-info'>Waiting...</h1>
 
