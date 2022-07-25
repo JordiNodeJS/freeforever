ac4af33bc6dbecebfe1fe4091d407a5bd67fb8fa
JordiNodeJS <71975008+JordiNodeJS@users.noreply.github.com>
2022-07-25 12:28:09 +0200

    firestore userCollectionRef

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -38,25 +38,12 @@ const SideBar = () => {
           <button onClick={handleAddEntry} className='active text-xl'>
             <AiOutlinePlusCircle />
           </button>
-          <div className='avatar flex-row items-center'>
-            <div className='w-8 rounded-full'>{ photoURL && <img  src={photoURL} />}</div>
-            <div className='mt-2 ml-2 font-thin'>{name}</div>
+          <div className='flex-row items-center flex-wrap'>
+            <div className='avatar w-8 rounded-full'>{ photoURL && <img  src={ photoURL } alt='foto perfil' />}</div>
+            
+            <div className='ml-2 font-thin'>{name}</div>
           </div>
-          {/* <button>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              className='h-5 w-5'
-              fill='none'
-              viewBox='0 0 24 24'
-              stroke='currentColor'>
-              <path
-                strokeLinecap='round'
-                strokeLinejoin='round'
-                strokeWidth={2}
-                d='M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z'
-              />
-            </svg>
-          </button> */}
+
           <AiOutlineLogout
             onClick={handleLogout}
             type='button'
